655aa51e370b182cab3786d9e143717785188987
Vinay Kaushik <kumar.vinay64@gmail.com>
2018-08-17 14:44:38 +0530

    create book donation project added

---
+++ src/main/java/org/bookdonation/BookdonationApplication.java
@@ -0,0 +1,12 @@
+package org.bookdonation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookdonationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookdonationApplication.class, args);
+	}
+}
